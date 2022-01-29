ebbfdcf1103591b1e698d1fac2ee9fe6d1339a10
Torbjörn Rathsman <milasudril@gmail.com>
2022-01-29 08:55:28 +0100

    Add color profiles

--- rgba_value.hpp
+++ rgba_value.hpp
@@ -1,18 +1,67 @@
 #ifndef PIXELSTORE_RGBAVALUE_HPP
 #define PIXELSTORE_RGBAVALUE_HPP
 
+#include <cmath>
+
 namespace pixel_store
 {
 	template<class T>
 	using vec4_t [[gnu::vector_size(4*sizeof(T))]] = T;
 
-	template<class T>
+	template<class To, class From>
+	constexpr To vector_cast(vec4_t<From> from)
+	{
+		return vec4_t<To>{static_cast<To>(from[0]),
+			static_cast<To>(from[1]),
+			static_cast<To>(from[2]),
+			static_cast<To>(from[3]),
+		};
+	}
+
+	namespace color_profiles
+	{
+		class linear
+		{
+			template<class T>
+			static constexpr auto to_linear(vec4_t<T> val) {return val;}
+
+			template<class T>
+			static constexpr auto from_linear(vec4_t<T> val) {return val;}
+		};
+
+		template<float Gamma>
+		class gamma
+		{
+			static constexpr auto to_linear(vec4_t<float> val)
+			{
+				return vec4_t<float>{std::pow(val[0], 1.0f/Gamma),
+					std::pow(val[1], 1.0f/Gamma),
+					std::pow(val[2], 1.0f/Gamma),
+					std::pow(val[3], 1.0f/Gamma)};
+			}
+
+			static constexpr auto from_linear(vec4_t<float> val)
+			{
+				return vec4_t<float>{std::pow(val[0], Gamma),
+					std::pow(val[1], Gamma),
+					std::pow(val[2], Gamma),
+					std::pow(val[3], Gamma)};
+			}
+		};
+	}
+
+	template<class T = float, class ColorProfile = color_profiles::linear>
 	class rgba_value
 	{
 	public:
+		using value_type = T;
+		using color_profile = ColorProfile;
+
         rgba_value() = default;
 
 		constexpr explicit rgba_value(T r, T g, T b, T a):m_value{r, g, b, a}{}
+
+		constexpr explicit rgba_value(vec4_t<T> val): m_value{val}{}
 
 		constexpr T red() const
 		{ return m_value[0]; }
@@ -49,30 +98,6 @@ namespace pixel_store
 	};
 
 	template<class T>
-	constexpr auto red_v = rgba_value<T>{1, 0, 0, 1};
-
-	template<class T>
-	constexpr auto green_v = rgba_value<T>{0, 1, 0, 1};
-
-	template<class T>
-	constexpr auto blue_v = rgba_value<T>{0, 0, 1, 1};
-
-	template<class T>
-	constexpr auto white_v rgba_value<T>{1, 1, 1, 1};
-
-	template<class T>
-	constexpr auto cyan_v = rgba_value<T>{0, 1, 1, 1};
-
-	template<class T>
-	constexpr auto magenta_v = rgba_value<T>{1, 0, 1, 1};
-
-	template<class T>
-	constexpr auto yellow_v = rgba_value<T>{1, 1, 0, 1};
-
-	template<class T>
-	constexpr auto black_v rgba_value<T>{0, 0, 0, 1};
-
-	template<class T>
 	constexpr auto operator==(rgba_value<T> a, rgba_value<T> b)
 	{
 		auto const val = a.value() - b.value();
@@ -83,6 +108,52 @@ namespace pixel_store
 	constexpr auto operator!=(rgba_value<T> a, rgba_value<T> b)
 	{
 		return !(a == b);
+	}
+
+	template<class To>
+	constexpr To convert_to(rgba_value<float> from);
+
+	template<>
+	constexpr auto convert_to<rgba_value<uint8_t, color_profiles::Gamma<2.2f>>>(rgba_value<float> from)
+	{
+		using profile = color_profiles::Gamma<2.2f>;
+		auto val = 255.0f*profile::from_linear(from);
+		return rgba_value{vector_cast<uint8_t>(val)};
+	}
+
+	template<>
+	cosntexpr auto convert_to<rgba_value<>>(rgba_value<uint8_t, color_profiles::Gamma<2.2f>> from)
+	{
+		auto val = vector_cast<float>(from.value())/255.0f;
+		using profile = color_profiles::Gamma<2.2f>;
+		return rgba_value<>{profile::to_linear(val)};
+	}
+
+	namespace standard_colors
+	{
+		template<class T>
+		constexpr auto red_v = rgba_value<T>{1, 0, 0, 1};
+
+		template<class T>
+		constexpr auto green_v = rgba_value<T>{0, 1, 0, 1};
+
+		template<class T>
+		constexpr auto blue_v = rgba_value<T>{0, 0, 1, 1};
+
+		template<class T>
+		constexpr auto white_v rgba_value<T>{1, 1, 1, 1};
+
+		template<class T>
+		constexpr auto cyan_v = rgba_value<T>{0, 1, 1, 1};
+
+		template<class T>
+		constexpr auto magenta_v = rgba_value<T>{1, 0, 1, 1};
+
+		template<class T>
+		constexpr auto yellow_v = rgba_value<T>{1, 1, 0, 1};
+
+		template<class T>
+		constexpr auto black_v rgba_value<T>{0, 0, 0, 1};
 	}
 }
 
